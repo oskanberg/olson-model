ce7c3a9e16ad8d6f82aa1b5b026d6ad3c1a34c1c
Oliver Skanberg-Tippen <ost500@york.ac.uk>
2014-11-04 16:24:35 +0000

    return pointers to Vector2D

--- vector.go
+++ vector.go
@@ -6,9 +6,9 @@ type Vector2D struct {
 	x, y float64
 }
 
-func (s *Vector2D) Normalised() Vector2D {
+func (s *Vector2D) Normalised() *Vector2D {
 	mag := s.Magnitude()
-	return Vector2D{
+	return &Vector2D{
 		x: s.x / mag,
 		y: s.y / mag,
 	}
@@ -18,8 +18,8 @@ func (s *Vector2D) Magnitude() float64 {
 	return math.Sqrt((s.x * s.x) + (s.y * s.y))
 }
 
-func (s *Vector2D) Subtract(v *Vector2D) Vector2D {
-	return Vector2D{
+func (s *Vector2D) Subtract(v *Vector2D) *Vector2D {
+	return &Vector2D{
 		x: s.x - v.x,
 		y: s.y - v.y,
 	}
